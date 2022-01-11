3b4802d7a56358b62ce19864e375d069c49703f8
jasminecomputer <94010896+jasminecomputer@users.noreply.github.com>
2022-01-11 14:35:25 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://lilyjanjigian:3gals1jasemily@cluster0.lg9w3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const mongoConnectionURL = "mongodb+srv://lilyjanjigian:3gals1jasemily@cluster0.lg9w3.mongodb.net/Jasemily?retryWrites=true&w=majority
 ";
 // TODO change database name to the name you chose
 const databaseName = "Jasemily";
