b7bbf1146d3ffc2034152cd01d392abfe1267919
Jasmine Chen <jasminechen@dhcp-10-29-64-177.dyn.mit.edu>
2022-01-15 00:20:27 -0500

    single bet changes

--- client/src/components/modules/SingleBet.js
+++ client/src/components/modules/SingleBet.js
@@ -1,5 +1,18 @@
 import React from "react";
-
 import { Link } from "@reach/router";
 
 //SingleBet is a component that renders the creator and the content of a bet
+
+const SingleBet = (props) => {
+  return (
+    <div>
+      <SingleBet
+        creator_name={props.creator_name}
+        creator_id={props.creator_id}
+        content={props.content}
+      />
+    </div>
+  );
+};
+
+export default SingleBet;
