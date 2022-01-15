2af55d789fa47e49c5b75d55638eaacb02551c30
Lily Janjigian <lilyjanjigian@dhcp-10-29-93-117.dyn.mit.edu>
2022-01-14 20:52:38 -0500

    Global Feed yes bro

--- client/src/components/pages/GlobalFeed.js
+++ client/src/components/pages/GlobalFeed.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const GlobalFeed = (props) => {
-
+    return (
+        <div>
+            Hello there this is Global Feed
+        </div>
+    )
 }
 
 export default GlobalFeed;
