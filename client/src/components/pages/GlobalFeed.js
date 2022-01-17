d331a986e4aae7176a39014ea4648a55a428c55a
Lily Janjigian <lilyjanjigian@Lilys-MacBook-Pro.local>
2022-01-17 12:40:16 -0500

    Reversed bets

--- client/src/components/pages/GlobalFeed.js
+++ client/src/components/pages/GlobalFeed.js
@@ -1,6 +1,5 @@
 import { process_params } from "express/lib/router";
 import React, { useState, useEffect } from "react";
-import NavBar from "../modules/NavBar.js";
 import { get } from "../../utilities.js";
 import SingleBet from "../modules/SingleBet.js";
 import NewBet from "../modules/ComposeBet.js"; // later change it to {}
@@ -12,14 +11,16 @@ const GlobalFeed = (props) => {
   useEffect(() => {
     document.title = "Global Feed";
     get("/api/globalbets").then((betObjs) => {
-      setBets(betObjs); // an array of bet objects
+      let reversedBetObjs = betObjs.reverse();
+      setBets(reversedBetObjs); // an array of bet objects
     });
   }, []);
 
   let feedBets = null;
   if (bets.length !== 0) {
     feedBets = bets.map((betObj) => {
-      return <SingleBet creator_name={betObj.creator_name} content={betObj.content} />;
+      // return <SingleBet creator_id={betObj.creator_id} creator_name={betObj.creator_name} content={betObj.content} />;
+      return betObj
     }); //map takes in a function, which we will apply to every item in the array
   } else {
     feedBets = <div> no bets! </div>;
