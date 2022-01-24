3aca5d7fe624b787cf214968302e130c4e0a3187
Jasmine Chen <jasminechen@dhcp-10-31-97-250.dyn.mit.edu>
2022-01-23 22:53:56 -0500

    two options

--- client/src/components/modules/SingleGuess.js
+++ client/src/components/modules/SingleGuess.js
@@ -2,7 +2,7 @@ import React from "react";
 
 /**
  * Component to render a single guess
- * 
+ *
  * Proptypes
  * @param {string} _id of comment
  * @param {string} creator_name
@@ -11,46 +11,6 @@ import React from "react";
  */
 
 const SingleGuess = (props) => {
-    return (
-        <div >
-            my vote:
-        </div>
-    );
-}
+  return <div>my vote:</div>;
+};
 export default SingleGuess;
-
-let poll = {
-    question: "What's your favorite programming language?",
-    answers: [
-        "C","Java", "PHP", "Javascript"
-    ],
-    pollCount: 20,
-    answersWeight: [4,4,2,10],
-    selectedAnswer: -1
-
-
-};
-
-let pollDOM = {
-    question:document.querySelector(".poll .question"),
-    answers:document.querySelector(".poll .answers")
-
-};
-
-pollDOM.question.innerText = poll.question;
-poll.answers.innerHTML = poll.answers.map(function(answer,i){
-    return (
-        <div onclick="markAnswer('${i}')" >
-            ${answer}
-
-        </div>
-    );
-}).join("");
-
-function markAnswer(i){
-    poll.selectedAnswer = +1;
-    try {
-
-        document.querySelector(".poll .answers .answer .selected").classList.remove("selected")
-    } catch(msg) {}
-}
