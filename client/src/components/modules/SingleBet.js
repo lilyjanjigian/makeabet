24b36795aa8aeef95d3e2187ec83003bc92b0bf9
Jasmine Chen <jasminechen@Hope-Laptop.local>
2022-01-25 14:30:53 -0500

    changes

--- client/src/components/modules/SingleBet.js
+++ client/src/components/modules/SingleBet.js
@@ -5,36 +5,28 @@ import SingleVote from "./SingleVote.js";
 /*
 //SingleBet is a component that renders the creator and the content of a bet
 
-Proptypes 
-@param {string} _id of the story
-@param {string} creator_name
-@param {string} content of the story 
 */
 
 const SingleBet = (props) => {
-  const [members, setMembers] = useState([]);
-  const handleEvent = (event) => {
-    alert("button clicked for" + event);
-    return (
-      <div className="Card-container">
-        <div className="u-bold" className="Card-title">
-          {props.creator_name}
-        </div>
-        <div className="Card-betcontent"> {props.content} </div>
-        <div className="Card-options"></div>
-        <div>
-          Options
-          {props.options.map((opt) => (
-            <SingleVote key={opt.id} content={opt.name} onClick={handleEvent} />
-          ))}
-        </div>
-        <div>Posted on {props.time_posted} </div>
-        <div>Expires on {props.time_expired}</div>
-        <div>Point value: {props.point_value}</div>
-        <div> Resolved? {props.isresolved ? "true" : "false"} </div>
+  return (
+    <div className="Card-container">
+      <div className="u-bold" className="Card-title">
+        {props.creator_name}
       </div>
-    );
-  };
+      <div className="Card-betcontent"> {props.content} </div>
+      <div className="Card-options"></div>
+      <div>
+        Options
+        {props.options.map((opt) => (
+          <SingleVote key={opt.id} content={opt.name} />
+        ))}
+      </div>
+      <div>Posted on {props.time_posted} </div>
+      <div>Expires on {props.time_expired}</div>
+      <div>Point value: {props.point_value}</div>
+      <div> Resolved? {props.isresolved ? "true" : "false"} </div>
+    </div>
+  );
 };
 
 export default SingleBet;
